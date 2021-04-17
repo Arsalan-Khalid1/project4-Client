976acac6c7658af0ccfe0d22264e334cdb8a5963
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2021-04-17 06:21:45 +0500

    mapbox rendering map

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,28 +1,42 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from "react";
+import "./Map.css";
+import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
 
-import './Map.css';
+mapboxgl.workerClass = MapboxWorker;
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZGFyZWRlZnltZTMiLCJhIjoiY2tubDBpdHczMGdhZDJ4czU4ZW5xeTRmMCJ9.ubW9Oh92gaHTPx-RarqeOQ";
 
-const Map = props => {
-  const mapRef = useRef();
-  
+const Map = (props) => {
   const { center, zoom } = props;
-
+  const mapContainer = useRef();
+  const [lng, setLng] = useState(center.lng);
+  const [lat, setLat] = useState(center.lat);
+  const [zoomm, setZoomm] = useState(zoom);
   useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
       center: center,
-      zoom: zoom
+      zoom: zoom,
     });
-  
-    new window.google.maps.Marker({ position: center, map: map });
-  }, [center, zoom]);  
 
+    map.on("move", () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoomm(map.getZoom().toFixed(2));
+    });
+
+    return () => map.remove();
+  }, [center, zoom]);
   return (
-    <div
-      ref={mapRef}
-      className={`map ${props.className}`}
-      style={props.style}
-    ></div>
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoomm}
+      </div>
+      <div className={`map-container ${props.className}`} ref={mapContainer} />
+    </div>
   );
 };
-
 export default Map;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -6,12 +6,28 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
+      name: "Arslan Khalid",
+      image:
+        "https://scontent.flhe3-1.fna.fbcdn.net/v/t1.6435-9/120270313_1678786832325970_8139761353787146575_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHEI2hQwNjMu7DCMryNTd5Nd7Ukq5Rsj1J3tSSrlGyPUgVk7mbR9JkS54Hf2O-J8pZTj-S1GraBz6K4IT-v7_XV&_nc_ohc=8WI16bhmwNAAX9cLmar&_nc_ht=scontent.flhe3-1.fna&oh=2bb920db3ebd366cd00ebab101ff2955&oe=60A07FE2",
+      places: 3,
+    },
+    {
+      id: "u2",
       name: "Max Schwarz",
       image:
         "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       places: 3,
     },
   ];
+  // const USERS = [
+  //   {
+  //     id: "u1",
+  //     name: "Max Schwarz",
+  //     image:
+  //       "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //     places: 3,
+  //   },
+  // ];
 
   return <UsersList items={USERS} />;
 };
