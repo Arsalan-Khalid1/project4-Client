018b1ce2ce31e74dde5e0cb9717c86e40212f7ec
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2021-04-21 09:46:11 +0500

    connected frontend and backend

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,18 +1,13 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 import PlaceList from "../components/PlaceList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import { useHttpClient } from "../../shared/hooks/http-hook";
-import { AuthContext } from "../../shared/context/auth-context";
 const UserPlaces = () => {
   const [loadedPlaces, setLoadedPlaces] = useState();
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const auth = useContext(AuthContext);
-  const userId = auth.userId;
-
-  console.log(userId);
+  const userId = useParams().userId;
   useEffect(() => {
     const fetchPlaces = async () => {
       try {
